44507508ca85a1002a1ac1ff36622fadf743b437
AlexJr003 <alexandre.cirino003@gmail.com>
2022-05-30 22:39:10 -0300

    final commit

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,63 +1,31 @@
-import React, {useState} from 'react' // importando o useState pra gerenciar o estado da nossa aplicação 
+import React, {useState} from 'react'
 import "./PaletaLista.css"
 import { paletas } from "../../mocks/paletas"
 
 console.log("paletas", paletas)
 
-// paletaSelecionadas = 
-// {
-//     "0": 4, 
-//     "5": 1, 
-//     "6": 2, 
-//     }
-
 function PaletaLista(){
 
     const [paletaSelecionadas, setPaletaSelecionadas ] = useState({})
 
-    // variável que guarda o valor do meu state
-    // método que atualiza o meu state 
-    // valor inicial dessa variável 
-
     function adicionarItem(index){
-        // const initialValue = paletaSelecionadas[index] ? paletaSelecionadas[index] : 0 
         const initialValue = Number(paletaSelecionadas[index] || 0) 
-        // o operador "||" retorna o valor indicado (0), caso a primeira condição seja falsa, ou seja, se não encontrar 
-        // o index da paleta clicada no obj paletas selecionadas
         const paleta = {
             [index]: initialValue + 1
         }
-        // Spread operator = espalhar => ...
+   
         setPaletaSelecionadas({...paletaSelecionadas, ...paleta})
     }
 
     function removerItem(index){
         const initialValue = Number(paletaSelecionadas[index] || 0) 
-        // if(initialValue > 0){
-        //     const paleta = {
-        //         [index]: initialValue - 1
-        //     }
-        //     // Spread operator = espalhar => ...
-        //     setPaletaSelecionadas({...paletaSelecionadas, ...paleta})
-        // }
+
         const paleta = {
             [index]: initialValue - 1
         }
-        // Spread operator = espalhar => ...
+
         setPaletaSelecionadas({...paletaSelecionadas, ...paleta})
     }
-
-    // OPÇÃO TERNARIO 
-    // const badgeCounter = (canRender, index) => {
-    //     return (
-    //         <> 
-    //             {Boolean(canRender)
-    //                 ? <span className="PaletaListaItem__badge">{paletaSelecionadas[index]}</span>
-    //                 : null 
-    //             }
-    //         </>
-    //         )
-    // }
 
     const badgeCounter2 = (canRender, index) => {
         return (
